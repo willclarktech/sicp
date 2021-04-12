07ad573856b858739cf7364ba0134032d29ac957
willclarktech <willclarktech@users.noreply.github.com>
2021-04-12 16:44:20 +0200

    Add Ex 1.3

---
+++ exercises/1.3.scm
@@ -0,0 +1,9 @@
+(define (sum-of-squares a b) (+ (square a) (square b)))
+
+(define (f a b c)
+	(cond
+		((and (<= a b) (<= a c)) (sum-of-squares b c))
+		((and (<= b a) (<= b c)) (sum-of-squares a c))
+		(else (sum-of-squares a b))
+	)
+)
